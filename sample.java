4ce0e0404f90c84cd97abb6e08b44126656ea98d
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2021-10-16 16:14:12 +0530

    Adding the Elements in arraylist using add method
    This program shows Adding the Elements in arraylist using add method

---
+++ sample.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class sample {
+	
+	public static void main(String args[]) {
+		ArrayList<String> arraylist = new ArrayList<>();
+		
+arraylist.add("vaishnavi");
+		
+arraylist.add("Avinash");
+		
+arraylist.add("Saindane");
+		
+System.out.println("Elements Add in Arraylist Display below:-");
+		
+System.out.println();
+
+for(String al:arraylist) {
+			System.out.print(al+" ");
+		}
+	      }
+}
